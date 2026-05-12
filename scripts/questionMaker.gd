efefdbfd5c74ef0d6786b3e7485c500908cb1efb
Axel <axel.riou405@gmail.com>
2026-05-12 17:00:09 +0200

    some optimisation + let player choose right answer WIP

--- scripts/questionMaker.gd
+++ scripts/questionMaker.gd
@@ -1,18 +1,25 @@
 extends Node
 @export var buttonCreateQuestion : Button
 @export var buttonCreateScene : Button
+@export var buttonAddImage : Button
 @export var textEdit : TextEdit
 @export var allTextEdit: Node2D
 
 @onready var file_dialog = $FileDialog
-@onready var preview = $TextureRect
+@onready var option_button = $OptionButton
+@onready var lineEdit = [ $TextEdit/Answer1Edit, $TextEdit/Answer2Edit, $TextEdit/Answer3Edit, $TextEdit/Answer4Edit]
 
 var selected_image_path := ""
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	buttonCreateQuestion.pressed.connect(CreateQuestion)
 	buttonCreateScene.pressed.connect(CreateScene)
+	buttonAddImage.pressed.connect(PutVisibleFileDialog)
 	changeNodeVisibility(false)
+	for i in range(lineEdit.size()):
+		lineEdit[i].text_changed.connect(_on_answer_edit_text_changed.bind(i))
+		if i > 0:
+			lineEdit[i].set_visible(false)
 	pass # Replace with function body.
 
 
@@ -28,11 +35,21 @@ func CreateScene():
 	var textNodeQuestion = sceneQuestion.get_node("Question/Texte/texte_personnalise")
 	if (textEdit.text != ""):
 		textNodeQuestion.text = textEdit.text
-		var index = 0
-		for textEdit in allTextEdit.get_children():
-			sceneQuestion.buttons[index-1].get_child(0).text = textEdit.text
-			index += 1
+	var index = 0
+	for text in allTextEdit.get_children():
+		if (text.text != ""):
+			sceneQuestion.buttons[index-1].get_child(0).text = text.text
+		index += 1
 		addInCsv(allTextEdit)
+	ImportAndAddImage(sceneQuestion)
+	
+func ImportAndAddImage(nodeSceneQuestion):
+	var image = Image.new()
+	var err = image.load(selected_image_path)
+	
+	if err == OK:
+		var texture = ImageTexture.create_from_image(image)
+		nodeSceneQuestion.get_node("TextureRect").texture = texture
 
 func CreateQuestion():
 	buttonCreateQuestion.hide()
@@ -53,20 +70,28 @@ func addInCsv(allStringsToAdd):
 		string.append('"%s"' % child.text)
 	csvFile.store_line(",".join(string))
 	csvFile.close()
-	
+
 func changeNodeVisibility(state):
 	buttonCreateScene.set_visible(state)
 	allTextEdit.set_visible(state)
-	
+	buttonAddImage.set_visible(state)
+	option_button.set_visible(state)
 
-func _on_button_pressed():
+
+func PutVisibleFileDialog():
 	file_dialog.popup_centered()
 
 func _on_file_dialog_file_selected(path):
 	selected_image_path = path
-	var image = Image.new()
-	var err = image.load(path)
-	
-	if err == OK:
-		var texture = ImageTexture.create_from_image(image)
-		preview.texture = texture
+	buttonAddImage.get_child(0).set_visible(true)
+
+
+func _on_answer_edit_text_changed(new_text, index) -> void:
+	if option_button.item > index:
+		option_button.set_item_text(index, new_text)
+	else:
+		option_button.add_item(new_text, index)
+	pass # Replace with function body.
+
+func AddAnAnswer():
+	pass
--- scripts/question_manager.gd
+++ scripts/question_manager.gd
@@ -12,42 +12,18 @@ extends Node2D
 func _ready():
 	for button in buttons:
 		button.pressed.connect(_on_button_pressed.bind(button))
-	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
-	#if Engine.is_editor_hint():
-		#_changeNodesOfACategoryState(nodetxt, "texte_personnalise", "texte_generique", texte)
 
-func _changeNodesOfACategoryState(_nodesCategory, stringNodeCustoms, stringNodeGen, boolCategory):
-	for child in _nodesCategory:
-		var allNodesOfCategoryGenArray = child.find_children(stringNodeCustoms)
-		var allNodesOfCategoryPlaceholderArray = child.find_children(stringNodeGen)
-		for element in allNodesOfCategoryGenArray:
-			if boolCategory:
-				element.set_visible(true)
-			else:
-				BonneRéponse = 2
-				element.set_visible(false)
-		for element in allNodesOfCategoryPlaceholderArray:
-			if boolCategory:
-				element.set_visible(false)
-			else:
-				BonneRéponse = 2
-				element.set_visible(true)
-
-func _changeNodeState(_node, _newState):
-	_node.set_visible(_newState)
-
-
-func _on_button_pressed(_button : Button):
+func _on_button_pressed(_button : TextureButton):
 	var index = buttons.find(_button, 0)
 	var _spritesNode
 	if index+1 == BonneRéponse:
 		_spritesNode = _button.get_parent().find_child("Bonne Réponse")
 	else:
 		_spritesNode = _button.get_parent().find_child("Mauvaise Réponse")
-	_button.icon = _spritesNode.texture
+	_button.texture_normal = _spritesNode.texture
 	for button in buttons:
-		button.pressed.disconnect(_on_button_pressed.bind(button))
+		button.disabled = true
